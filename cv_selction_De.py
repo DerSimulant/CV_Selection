4246d11bec2723cc86bdfd77212fb4d289d5f63d
Sebastian Kotulla <82386236+DerSimulant@users.noreply.github.com>
2023-10-26 08:49:49 +0200

    Create cv_selction_De.py
    Deutsche Kommentare zur Nachvollziehbarkeit

---
+++ cv_selction_De.py
@@ -0,0 +1,97 @@
+import os
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+# Initialisiere die Webcam
+cap = cv2.VideoCapture(1)
+cap.set(3,640)  # Setze die Breite des Kamerabilds
+cap.set(4,480)  # Setze die Höhe des Kamerabilds
+
+# Lade das Hintergrundbild
+imgBackground = cv2.imread("Resources/Background.png")
+
+# Lade die Bilder für die verschiedenen Modi in eine Liste
+folderPathModes = "Resources/Modes"
+listImgModesPath = os.listdir(folderPathModes)
+listImgModes = [cv2.imread(os.path.join(folderPathModes, imgModePath)) for imgModePath in listImgModesPath]
+
+# Lade die Icons in eine Liste
+folderPathIcons = "Resources/Icons"
+listImgIconsPath = os.listdir(folderPathIcons)
+listImgIcons = [cv2.imread(os.path.join(folderPathIcons, imgIconPath)) for imgIconPath in listImgIconsPath]
+print(listImgIcons)
+
+# Initialisiere Variablen
+modeType = 0  # Hier kannst du die Auswahl des Modus ändern
+selection = -1  # Ist eine Auswahl getroffen?
+counter = 0  # Zähler, um nicht direkt auszuwählen, sondern erst nach einer gewissen Zeit
+selectionSpeed = 7  # Multiplikator, um den Auswahlkreis schneller zu schließen
+detector = HandDetector(detectionCon=0.8, maxHands=1)  # Initialisiere den Handdetektor
+modePositions = [(1136, 196),(999,389),(1140,586)]  # Positionen der Auswahlmöglichkeiten
+counterPause = 0  # Pausenzähler nach jeder Auswahl
+selectionList = [-1,-1,-1]  # Liste der getroffenen Auswahlen
+
+# Hauptprogrammschleife
+while True:
+    # Lese Kamerabild
+    success, img = cap.read()
+
+    # Finde die Hände und ihre Landmarken im Bild
+    hands, img = detector.findHands(img)  # mit Zeichnung der Hand
+    # hands = detector.findHands(img, draw=False)  # ohne Zeichnung der Hand
+
+    # Überlagere das Kamerabild auf den Hintergrund
+    imgBackground[139:139+480, 49:49+640] = img
+    imgBackground[0:720, 847:1280] = listImgModes[modeType]
+
+    # Handerkennung und Auswahllogik
+    if hands and counterPause == 0 and modeType<3:
+        hand1 = hands[0]  # Erste erkannte Hand
+        fingers1 = detector.fingersUp(hand1)  # Zustand der Finger der ersten Hand
+        print(fingers1)
+
+        # Auswahllogik basierend auf den hochgestreckten Fingern
+        if fingers1 == [0,1,0,0,0]:
+            if selection !=1:
+                counter = 1
+            selection = 1
+        elif fingers1 == [0,1,1,0,0]:
+            if selection !=2:
+                counter = 1
+            selection = 2
+        elif fingers1 == [0,1,0,0,1]:
+            if selection !=3:
+                counter = 1
+            selection = 3
+        else:
+            selection =-1
+            counter = 0
+
+        # Zeichne den Auswahlkreis und überprüfe, ob die Auswahl getroffen wurde
+        if counter > 0:
+            counter += 1
+            print(counter)
+            cv2.ellipse(imgBackground, modePositions[selection-1], (103,103), 0, 0, counter*selectionSpeed, (0,255,0), 20)
+            if counter*selectionSpeed > 360:
+                selectionList[modeType] = selection
+                modeType += 1
+                counter = 0
+                selection = -1
+                counterPause = 1
+
+    # Pausiere nach jeder Auswahl
+    if counterPause > 0:
+        counterPause += 1
+        if counterPause > 40:
+            counterPause = 0
+
+    # Füge das Auswahl-Icon unten hinzu
+    for i in range(3):
+        if selectionList[i] != -1:
+            imgBackground[636:636+65, 133+207*i:133+207*i+65] = listImgIcons[2*i+selectionList[i]-1]
+
+    # Zeige das Bild an
+    cv2.imshow("Dat Bild Digger", imgBackground)
+
+    # Warte auf Tastendruck (für interaktive Steuerung oder Programmbeendigung)
+    cv2.waitKey(1)
